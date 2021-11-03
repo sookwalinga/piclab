2d9f015b05e5cf48fc89ace02dfe0abaefcdcfeb
Samuel Okwalinga <sookwalinga@gmail.com>
2021-11-03 22:24:04 +0300

    Complete section 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e9dcd9ddb6354c7"; }
+/******/ 		__webpack_require__.h = function() { return "92e05854263451db"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -7,89 +7,172 @@ import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-r from-primary to-secondary font-Inter text-white'>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-r from-primary via-blue-900 to-secondary font-Inter text-white'>
       <Head>
         <title>Home</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className='flex flex-row  w-full'>
-        <div className='flex-2 px-20 pt-20'>
-          <div className='flex flex-row  w-full pb-36 '>
-            <Image
-              src='/logo.png'
-              alt='Logo'
-              // layout='fill'
-              width={20}
-              height={40}
-            />
-            <h1 className='pl-2 pt-2 font-bold'>PICLAB</h1>
-          </div>
-
-          <button className='border-2 rounded-lg border-black-500 px-2 text-xs font-Idlewild'>
-            COMING SUMMER 2021
-          </button>
-          <p className='pt-6 text-3xl'>Powerful image</p>
-          <p className='text-3xl font-bold'>asset management</p>
-          <p className='text-3xl'>for everyone.</p>
-          <p className='pt-20 mr-8'>
-            <div className='border-l-2  border-black-500 h-20 w-72 text-sm'>
-              {' '}
-              <div className='ml-8'>
-                <span className='font-bold'>Organize, transform, & serve</span>
-                your entire photo library. The perfect image management app for
-                freelancers, organizations & web developers.
-              </div>
+      <main className='flex flex-col  w-full'>
+        <div className='flex flex-row  w-full'>
+          <div className='flex-2 px-20 pt-20'>
+            <div className='flex flex-row  w-full pb-36 '>
+              <Image
+                src='/logo.png'
+                alt='Logo'
+                // layout='fill'
+                width={20}
+                height={40}
+              />
+              <h1 className='pl-2 pt-2 font-bold'>PICLAB</h1>
             </div>
-          </p>
+
+            <button className='border-2 rounded-lg border-black-500 px-2 text-xs font-Idlewild'>
+              COMING SUMMER 2021
+            </button>
+            <p className='pt-6 text-3xl'>Powerful image</p>
+            <p className='text-3xl font-bold'>asset management</p>
+            <p className='text-3xl'>for everyone.</p>
+            <p className='pt-20 mr-8'>
+              <div className='border-l-2  border-black-500 h-20 w-72 text-sm'>
+                <div className='ml-8'>
+                  <span className='font-bold mr-1'>
+                    Organize, transform, & serve
+                  </span>
+                  your entire photo library. The perfect image management app
+                  for freelancers, organizations & web developers.
+                </div>
+              </div>
+            </p>
+          </div>
+          <div className='flex-3 pt-20'>
+            <img
+              className='w-screen shadow rounded max-w-full h-screen align-middle border-none'
+              src='/apple-macbook.png'
+              alt='image'
+            />
+          </div>
+          <div className='fixed bottom-0  mb-2 bg-white rounded-lg justify-evenly flex w-full p-2 z-10'>
+            <div className='text-black pr-56'>
+              <form
+                action='
+          #'
+                method='post'
+              >
+                <div className='flex border-2 rounded-lg px-6'>
+                  <input
+                    type='email'
+                    name='client-email'
+                    id='client-email'
+                    className='p-2'
+                    placeholder='Enter email'
+                  />
+
+                  <span className='whitespace-nowrap pt-2 text-pink-600'>
+                    | <span className='pl-8'> Request Beta</span>
+                  </span>
+                </div>
+              </form>
+            </div>
+            <div className='flex flex-row  w-full text-black pl-10 '>
+              <Image
+                src='/logo-red.png'
+                alt='Logo'
+                // layout='fill'
+                width={10}
+                height={20}
+              />
+              <h1 className='pl-2 pt-2'>PICLAB</h1>
+            </div>
+            <button
+              type='submit'
+              className='text-pink-600 mr-10 whitespace-nowrap'
+            >
+              <FontAwesomeIcon icon={faSignInAlt} size='lg' />
+              <span className='pl-2'> Sign In</span>
+            </button>
+          </div>
         </div>
-        <div className='flex-3 pt-20'>
+        <div className='flex flex-row  w-full bg-gradient-to-r from-black to-blue-900 pt-20'>
           <img
-            className='w-screen shadow rounded max-w-full h-screen align-middle border-none'
-            src='/apple-macbook.png'
+            className='w-screen shadow rounded max-w-20 h-screen align-middle border-none pr-72'
+            src='/asset2.png'
             alt='image'
           />
-        </div>
-        <div className='fixed bottom-0 mb-2 bg-white rounded-lg justify-evenly flex w-full p-2'>
-          <div className='text-black pr-56'>
-            <form
-              action='
-          #'
-              method='post'
-            >
-              <div className='flex border-2 rounded-lg px-6'>
-                <input
-                  type='email'
-                  name='client-email'
-                  id='client-email'
-                  className='p-2'
-                  placeholder='Enter email'
-                />
-
-                <span className='whitespace-nowrap pt-2 text-pink-600'>
-                  | <span className='pl-8'> Request Beta</span>
-                </span>
-              </div>
-            </form>
-          </div>
-          <div className='flex flex-row  w-full text-black pl-10 '>
-            <Image
-              src='/logo-red.png'
-              alt='Logo'
-              // layout='fill'
-              width={10}
-              height={20}
+          <div className='sticky bottom-0 right-20 mb-0 bg-white rounded-lg justify-evenly pr-10 pl-10 pt-8'>
+            <div>
+              <p className='text-gray-400 whitespace-nowrap '>
+                Artificial Intelligence
+              </p>
+            </div>
+            <div className='text-4xl font-medium'>
+              <p className='text-gray-600 whitespace-nowrap pt-10 '>
+                Let <span className='text-pink-600'>A.I.</span> handle lorem
+              </p>
+              <p className='text-gray-600 whitespace-nowrap'>
+                ipsum dolor sit.
+              </p>
+            </div>
+            <img
+              className='w-36 shadow rounded h-auto align-middle border-none mx-auto my-20'
+              src='/ai.png'
+              alt='image'
             />
-            <h1 className='pl-2 pt-2'>PICLAB</h1>
+
+            <div className='text-2xl font-normal'>
+              <p className='text-gray-400 whitespace-nowrap'>
+                <span className='text-gray-600'> No more need to </span> lorem ipsum dolor sit
+              </p>
+              <p className='text-gray-400 whitespace-nowrap'>
+                amet, consectetur adipiscing elit, sed
+              </p>
+              <p className='text-gray-400 whitespace-nowrap'>
+                do eiusmod tempor incididunt ut labore
+              </p>
+              <p className='text-gray-400 whitespace-nowrap'>
+                et dolore magna.
+              </p>
+            </div>
           </div>
-          <button
-            type='submit'
-            className='text-pink-600 mr-10 whitespace-nowrap'
-          >
-            <FontAwesomeIcon icon={faSignInAlt} size='lg' />
-            <span className='pl-2'> Sign In</span>
-          </button>
         </div>
+        <div className='flex flex-row  w-full bg-gradient-to-r from-primary to-secondary pt-20'>
+          <img
+            className='w-screen shadow rounded max-w-20 h-screen align-middle border-none pr-72'
+            src='/asset2.png'
+            alt='image'
+          />
+          <div className='sticky bottom-0 right-20 mb-0 bg-white rounded-lg justify-evenly pr-10 pl-10 pt-8'>
+            <div>
+              <p className='text-gray-400 whitespace-nowrap '>
+                Artificial Intelligence
+              </p>
+            </div>
+            <div className='text-4xl font-medium'>
+              <p className='text-gray-600 whitespace-nowrap pt-10 '>
+                Let <span className='text-pink-600'>A.I.</span> handle lorem
+              </p>
+              <p className='text-gray-600 whitespace-nowrap'>
+                ipsum dolor sit.
+              </p>
+            </div>
+
+            <div className='text-2xl font-normal'>
+              <p className='text-gray-400 whitespace-nowrap'>
+                <span className='text-gray-600'> No more need to </span> lorem ipsum dolor sit
+              </p>
+              <p className='text-gray-400 whitespace-nowrap'>
+                amet, consectetur adipiscing elit, sed
+              </p>
+              <p className='text-gray-400 whitespace-nowrap'>
+                do eiusmod tempor incididunt ut labore
+              </p>
+              <p className='text-gray-400 whitespace-nowrap'>
+                et dolore magna.
+              </p>
+            </div>
+          </div>
+        </div>
+
       </main>
 
       <footer className='flex items-center justify-center w-full h-24 border-t'>
