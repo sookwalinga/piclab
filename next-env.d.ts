f105a7dddf299cb528b5ca5da00ac92dfc679238
SamuelOkwalinga <sookwalinga@gmail.com>
2021-12-10 17:33:22 +0300

    Merge dashboard branch to main

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,8 +1,4 @@
 /// <reference types="next" />
-<<<<<<< HEAD
-/// <reference types="next/types/global" />
-=======
->>>>>>> 6646367f54ead79d459799ec6588e3c1ce80f378
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
